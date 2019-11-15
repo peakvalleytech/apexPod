peakvalleytech/apexPod

commit 1e771e3709c8f3050896cd4180273e9eea2299c6

Fix error-prone errors

core/src/main/java/de/danoeh/antennapod/core/feed/FeedItem.java
@@ -194,7 +194,7 @@ public class FeedItem extends FeedComponent implements ShownotesProvider, Flattr
         if (other.link != null) {
             link = other.link;
         }
-        if (other.pubDate != null && other.pubDate != pubDate) {
+        if (other.pubDate != null && other.pubDate.equals(pubDate)) {
             pubDate = other.pubDate;
         }
         if (other.media != null) {

app/src/main/java/de/danoeh/antennapod/PodcastApp.java
@@ -20,7 +20,7 @@ public class PodcastApp extends Application {
         try {
             Class.forName("de.danoeh.antennapod.config.ClientConfigurator");
         } catch (Exception e) {
-            throw new RuntimeException("ClientConfigurator not found");
+            throw new RuntimeException("ClientConfigurator not found", e);
         }
     }
 

app/src/main/java/de/danoeh/antennapod/activity/FeedSettingsActivity.java
@@ -69,22 +69,6 @@ public class FeedSettingsActivity extends AppCompatActivity {
 
     private Subscription subscription;
 
-
-    private final View.OnClickListener copyUrlToClipboard = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            if(feed != null && feed.getDownload_url() != null) {
-                String url = feed.getDownload_url();
-                ClipData clipData = ClipData.newPlainText(url, url);
-                android.content.ClipboardManager cm = (android.content.ClipboardManager) FeedSettingsActivity.this
-                        .getSystemService(Context.CLIPBOARD_SERVICE);
-                cm.setPrimaryClip(clipData);
-                Toast t = Toast.makeText(FeedSettingsActivity.this, R.string.copied_url_msg, Toast.LENGTH_SHORT);
-                t.show();
-            }
-        }
-    };
-
     private boolean authInfoChanged = false;
 
     private final TextWatcher authTextWatcher = new TextWatcher() {

app/src/main/java/de/danoeh/antennapod/activity/MediaplayerActivity.java
@@ -231,7 +231,6 @@ public abstract class MediaplayerActivity extends CastEnabledActivity implements
         Log.d(TAG, "onCreate()");
         StorageUtils.checkStorageAvailability(this);
 
-        orientation = getResources().getConfiguration().orientation;
         getWindow().setFormat(PixelFormat.TRANSPARENT);
     }
 
@@ -265,14 +264,9 @@ public abstract class MediaplayerActivity extends CastEnabledActivity implements
 
     private void onBufferUpdate(float progress) {
         if (sbPosition != null) {
-            sbPosition.setSecondaryProgress((int) progress * sbPosition.getMax());
+            sbPosition.setSecondaryProgress((int) (progress * sbPosition.getMax()));
         }
     }
-
-    /**
-     * Current screen orientation.
-     */
-    private int orientation;
 
     @Override
     protected void onStart() {
@@ -992,7 +986,7 @@ public abstract class MediaplayerActivity extends CastEnabledActivity implements
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == REQUEST_CODE_STORAGE) {
             if (grantResults.length <= 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, R.string.needs_storage_permission, Toast.LENGTH_LONG).show();

app/src/main/java/de/danoeh/antennapod/activity/MediaplayerInfoActivity.java
@@ -357,7 +357,7 @@ public abstract class MediaplayerInfoActivity extends MediaplayerActivity implem
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return drawerToggle != null && drawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+        return (drawerToggle != null && drawerToggle.onOptionsItemSelected(item)) || super.onOptionsItemSelected(item);
     }
 
     @Override

app/src/main/java/de/danoeh/antennapod/activity/OnlineFeedViewActivity.java
@@ -144,7 +144,7 @@ public class OnlineFeedViewActivity extends AppCompatActivity {
             feedUrl = getIntent().getStringExtra(ARG_FEEDURL);
         } else if (TextUtils.equals(getIntent().getAction(), Intent.ACTION_SEND)
                 || TextUtils.equals(getIntent().getAction(), Intent.ACTION_VIEW)) {
-            feedUrl = (TextUtils.equals(getIntent().getAction(), Intent.ACTION_SEND))
+            feedUrl = TextUtils.equals(getIntent().getAction(), Intent.ACTION_SEND)
                     ? getIntent().getStringExtra(Intent.EXTRA_TEXT) : getIntent().getDataString();
             if (actionBar != null) {
                 actionBar.setTitle(R.string.add_feed_label);
@@ -306,7 +306,7 @@ public class OnlineFeedViewActivity extends AppCompatActivity {
     }
 
     private void parseFeed() {
-        if (feed == null || feed.getFile_url() == null && feed.isDownloaded()) {
+        if (feed == null || (feed.getFile_url() == null && feed.isDownloaded())) {
             throw new IllegalStateException("feed must be non-null and downloaded when parseFeed is called");
         }
         Log.d(TAG, "Parsing feed");

app/src/main/java/de/danoeh/antennapod/activity/gpoddernet/GpodnetAuthenticationActivity.java
@@ -45,8 +45,6 @@ import de.danoeh.antennapod.core.service.GpodnetSyncService;
 public class GpodnetAuthenticationActivity extends AppCompatActivity {
     private static final String TAG = "GpodnetAuthActivity";
 
-    private static final String CURRENT_STEP = "current_step";
-
     private ViewFlipper viewFlipper;
 
     private static final int STEP_DEFAULT = -1;

app/src/main/java/de/danoeh/antennapod/fragment/CoverFragment.java
@@ -22,7 +22,6 @@ import de.danoeh.antennapod.core.util.playback.Playable;
 public class CoverFragment extends Fragment implements MediaplayerInfoContentFragment {
 
     private static final String TAG = "CoverFragment";
-    private static final String ARG_PLAYABLE = "arg.playable";
 
     private Playable media;
 

commit 8e11c8ddebc696e4859a0db4d0ddddfc7a190d38

Fixed feeditem date update
Bug introduced in 1e771e3709c8f3050896cd4180273e9eea2299c6

core/src/main/java/de/danoeh/antennapod/core/feed/FeedItem.java
@@ -185,7 +185,7 @@ public class FeedItem extends FeedComponent implements ShownotesProvider, ImageR
         if (other.link != null) {
             link = other.link;
         }
-        if (other.pubDate != null && other.pubDate.equals(pubDate)) {
+        if (other.pubDate != null && !other.pubDate.equals(pubDate)) {
             pubDate = other.pubDate;
         }
         if (other.media != null) {