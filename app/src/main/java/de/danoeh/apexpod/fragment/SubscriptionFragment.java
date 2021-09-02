ff3f62aeebbb219c6ee0eef13490b5787b4a2eec
peakvalleytech <peakvalleytech@gmail.com>
2021-09-01 22:22:38 -0700

    Create new dialog for adding tags using multi select action.

--- app/src/main/java/de/danoeh/apexpod/fragment/SubscriptionFragment.java
+++ app/src/main/java/de/danoeh/apexpod/fragment/SubscriptionFragment.java
@@ -18,6 +18,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -346,6 +347,11 @@ public class SubscriptionFragment extends Fragment
             return true;
         } else if (itemId == R.id.edit_tags) {
             TagSettingsDialog.newInstance(feed.getPreferences()).show(getChildFragmentManager(), TagSettingsDialog.TAG);
+        } else if (itemId ==  R.id.mark_all_read_item) {
+            displayConfirmationDialog(
+                    R.string.mark_all_read_label,
+                    R.string.mark_all_read_confirmation_msg,
+                    () -> DBWriter.markFeedRead(feed.getId()));
             return true;
         } else if (itemId == R.id.rename_item) {
             new RenameFeedDialog(getActivity(), feed).show();
