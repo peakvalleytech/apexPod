3d8e0c78e80bd9d822d89ce70d80beaa02340708
peakvalleytech <peakvalleytech@gmail.com>
2021-09-25 23:39:00 -0700

    Replace dialog listener with onPreferenceChangedListener

--- app/src/main/java/de/danoeh/apexpod/fragment/AudioPlayerFragment.java
+++ app/src/main/java/de/danoeh/apexpod/fragment/AudioPlayerFragment.java
@@ -1,6 +1,7 @@
 package de.danoeh.apexpod.fragment;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,6 +21,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.interpolator.view.animation.FastOutSlowInInterpolator;
+import androidx.preference.PreferenceManager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -73,7 +75,7 @@ import io.reactivex.schedulers.Schedulers;
  * Shows the audio player.
  */
 public class AudioPlayerFragment extends Fragment implements
-        ChapterSeekBar.OnSeekBarChangeListener, Toolbar.OnMenuItemClickListener {
+        ChapterSeekBar.OnSeekBarChangeListener, Toolbar.OnMenuItemClickListener, SharedPreferences.OnSharedPreferenceChangeListener {
     public static final String TAG = "AudioPlayerFragment";
     public static final int POS_COVER = 0;
     public static final int POS_DESCRIPTION = 1;
@@ -159,6 +161,9 @@ public class AudioPlayerFragment extends Fragment implements
                 });
             }
         });
+
+        PreferenceManager.getDefaultSharedPreferences(getContext()).registerOnSharedPreferenceChangeListener(this);
+
 
         return root;
     }
@@ -527,14 +532,6 @@ public class AudioPlayerFragment extends Fragment implements
             return true;
         } else if (itemId == R.id.audio_controls) {
             PlaybackControlsDialog dialog = PlaybackControlsDialog.newInstance();
-            dialog.setOnRepeatChanged(repeat -> {
-                repeat = UserPreferences.getShouldRepeatEpisode();
-                if (repeat) {
-                    imgvRepeat.setVisibility(View.VISIBLE);
-                } else {
-                    imgvRepeat.setVisibility(View.GONE);
-                }
-            });
             dialog.show(getChildFragmentManager(), "playback_controls");
             return true;
         } else if (itemId == R.id.open_feed_item) {
@@ -546,6 +543,17 @@ public class AudioPlayerFragment extends Fragment implements
         }
 
         return false;
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (UserPreferences.PREF_REPEAT_EPISODE.equals(key)) {
+            if (UserPreferences.getShouldRepeatEpisode()) {
+                imgvRepeat.setVisibility(View.VISIBLE);
+            } else {
+                imgvRepeat.setVisibility(View.GONE);
+            }
+        }
     }
 
     private static class AudioPlayerPagerAdapter extends FragmentStateAdapter {
