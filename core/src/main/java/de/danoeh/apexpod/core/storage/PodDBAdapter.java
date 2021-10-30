fff96b208b531635d01a832b880cef46331f8c12
peakvalleytech <peakvalleytech@gmail.com>
2021-10-30 02:04:35 -0700

    Crate playlist table

--- core/src/main/java/de/danoeh/apexpod/core/storage/PodDBAdapter.java
+++ core/src/main/java/de/danoeh/apexpod/core/storage/PodDBAdapter.java
@@ -118,6 +118,7 @@ public class PodDBAdapter {
     public static final String KEY_FEED_SKIP_ENDING = "feed_skip_ending";
     public static final String KEY_FEED_TAGS = "tags";
     public static final String KEY_EPISODE_NOTIFICATION = "episode_notification";
+    public static final String KEY_PLAYLIST_NAME = "name";
 
     // Table names
     public static final String TABLE_NAME_FEEDS = "Feeds";
@@ -128,6 +129,7 @@ public class PodDBAdapter {
     public static final String TABLE_NAME_QUEUE = "Queue";
     public static final String TABLE_NAME_SIMPLECHAPTERS = "SimpleChapters";
     public static final String TABLE_NAME_FAVORITES = "Favorites";
+    public static final String TABLE_NAME_PLAYLIST = "Playlists";
 
     // SQL Statements for creating new tables
     private static final String TABLE_PRIMARY_KEY = KEY_ID
@@ -190,6 +192,13 @@ public class PodDBAdapter {
     private static final String CREATE_TABLE_QUEUE = "CREATE TABLE "
             + TABLE_NAME_QUEUE + "(" + KEY_ID + " INTEGER PRIMARY KEY,"
             + KEY_FEEDITEM + " INTEGER," + KEY_FEED + " INTEGER)";
+
+    private static final String CREATE_TABLE_PLAYLISTS = "CREATE TABLE "
+            + TABLE_NAME_PLAYLIST +
+            "(" + KEY_ID + "INTEGER PRIMARY KEY," +
+            KEY_PLAYLIST_NAME + "INTEGER" +
+            KEY_FEEDITEM + "INTEGER," +
+            KEY_FEED + " INTEGER)";
 
     private static final String CREATE_TABLE_SIMPLECHAPTERS = "CREATE TABLE "
             + TABLE_NAME_SIMPLECHAPTERS + " (" + TABLE_PRIMARY_KEY + KEY_TITLE
@@ -1433,6 +1442,7 @@ public class PodDBAdapter {
             db.execSQL(CREATE_TABLE_QUEUE);
             db.execSQL(CREATE_TABLE_SIMPLECHAPTERS);
             db.execSQL(CREATE_TABLE_FAVORITES);
+            db.execSQL(CREATE_TABLE_PLAYLISTS);
 
             db.execSQL(CREATE_INDEX_FEEDITEMS_FEED);
             db.execSQL(CREATE_INDEX_FEEDITEMS_PUBDATE);
