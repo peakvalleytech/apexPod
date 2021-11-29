0e985888f9bfd42858d3579c79242fa479a08cf8
peakvalleytech <peakvalleytech@gmail.com>
2021-11-28 18:09:02 -0800

    Add unit tests for AutoDownloadServiceImpl

--- core/src/main/java/de/danoeh/apexpod/core/storage/autodownload/impl/AutoDownloadServiceImpl.kt
+++ core/src/main/java/de/danoeh/apexpod/core/storage/autodownload/impl/AutoDownloadServiceImpl.kt
@@ -1,10 +1,14 @@
 package de.danoeh.apexpod.core.storage.autodownload.impl
 
 import android.content.Context
+import androidx.annotation.VisibleForTesting
 import de.danoeh.apexpod.core.preferences.UserPreferences
 import de.danoeh.apexpod.core.storage.DBReader
 import de.danoeh.apexpod.core.util.NetworkUtils
+import de.danoeh.apexpod.model.feed.AutoDownload
 import de.danoeh.apexpod.model.feed.Feed
+import de.danoeh.apexpod.model.feed.FeedItem
+import de.danoeh.apexpod.model.feed.FeedPreferences
 
 class AutoDownloadServiceImpl() {
     private val TAG = "AutoDownlaodService"
@@ -19,16 +23,60 @@ class AutoDownloadServiceImpl() {
 
                 for (feed in feeds) {
                     val preferences = feed.preferences
-                    if (preferences.autoDownload)
+                    if (preferences.autoDownload) {
                         // if getAll
-                            // get all episodes
-                            // sort for newest or oldest
-                            // Retrieve update count
-                            // Download if necessary
+                        // get all episodes
+                        // sort for newest or oldest
+                        // Retrieve update count
+                        // Download if necessary
                         // else
+                        val items = feed.items
+                        selectFeedItems(
+                            autodownloadprefs = preferences.autoDownloadPreferences,
+                            items
+                        )
+                    }
                 }
             }
         }
     }
 
+    companion object {
+        /**
+         * Selects the feed items to auto download
+         * @param autodownloadprefs the autodownloadprefs that holds auto download options
+         * used to deteremine which items to select
+         * @param feeditems the feeditems to select from
+         */
+        @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+        fun selectFeedItems(autodownloadprefs: AutoDownload, items: List<FeedItem>)
+        : List<FeedItem> {
+            var selectedItems = mutableListOf<FeedItem>()
+            if (!autodownloadprefs.isIncludeAll) {
+                items.forEach {
+                    if (it.playState == FeedItem.NEW) {
+                        selectedItems.add(it)
+                    }
+                }
+            } else {
+                items.forEach {
+                    if (it.playState == FeedItem.NEW
+                        || it.playState == FeedItem.UNPLAYED) {
+                        selectedItems.add(it)
+                    }
+                }
+            }
+            selectedItems.sortBy { feedItem -> feedItem.pubDate }
+            if (autodownloadprefs.isNewestFirst) {
+                selectedItems.reverse()
+            }
+
+            if (autodownloadprefs.numberToUpdate > selectedItems.size) {
+                return selectedItems
+            } else  {
+                return selectedItems.subList(0, autodownloadprefs.numberToUpdate)
+            }
+            return selectedItems
+        }
+    }
 }
--- core/src/test/java/de/danoeh/apexpod/core/storage/autodownload/TestAutoDownloadService.kt
+++ core/src/test/java/de/danoeh/apexpod/core/storage/autodownload/TestAutoDownloadService.kt
@@ -1,25 +1,99 @@
 package de.danoeh.apexpod.core.storage.autodownload
 
+import de.danoeh.apexpod.core.storage.autodownload.impl.AutoDownloadServiceImpl
+import de.danoeh.apexpod.model.feed.*
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
+import java.util.*
 
 class TestAutoDownloadService {
-    @Test
-    fun `should download newest N new episodes`() {
+    private lateinit var items : MutableList<FeedItem>
+    val feed = Feed("url", null, "title")
+    lateinit var feedPreferences : FeedPreferences
+    lateinit var feedItems : List<FeedItem>
+    @Before
+    fun setup(){
+        feedPreferences = FeedPreferences(
+            1,
+            true,
+            FeedPreferences.AutoDeleteAction.GLOBAL,
+            VolumeAdaptionSetting.OFF,
+            "",
+            ""
+        )
+        val newItem1 = FeedItem(
+            1, "title",
+            "item1", "link", Date(), FeedItem.NEW, feed
+        )
+        val newItem2 = FeedItem(
+            2, "title",
+            "item2", "link", Date(), FeedItem.NEW, feed
+        )
+        val oldItem1 = FeedItem(
+            3, "title",
+            "item1", "link", Date(), FeedItem.UNPLAYED, feed
+        )
+        val oldItem2 = FeedItem(
+            4, "title",
+            "item2", "link", Date(), FeedItem.UNPLAYED, feed
+        )
+        feedItems = listOf(newItem1, newItem2, oldItem1, oldItem2)
 
     }
 
+    /**
+     *
+     */
     @Test
-    fun `should download newest N episodes`() {
+    fun `when limit is lt number of new items select up to limit`() {
+        feedPreferences.autoDownloadPreferences = AutoDownload(1, true, false)
 
+        val selectedFeedItems = AutoDownloadServiceImpl.selectFeedItems(feedPreferences.autoDownloadPreferences,
+            feedItems
+            )
+        assertEquals(1, selectedFeedItems.size)
+        val expectedItemId = 2L
+        val actualItemId = selectedFeedItems.get(0).id
+        assertEquals(expectedItemId, actualItemId)
     }
 
     @Test
-    fun `should download oldest N new episodes`() {
-
+    fun `when limit gt number of new items select all new and unplayed items`() {
+        feedPreferences.autoDownloadPreferences = AutoDownload(4, true, false)
+        val selectedFeedItems = AutoDownloadServiceImpl.selectFeedItems(feedPreferences.autoDownloadPreferences,
+            feedItems
+        )
+        assertEquals(2, selectedFeedItems.size)
+        selectedFeedItems.forEach {
+            assertTrue(it.playState == FeedItem.NEW)
+        }
+    }
+    @Test
+    fun `when limit is lt number of unplayed items select up to limit`() {
+        feedPreferences.autoDownloadPreferences = AutoDownload(4, true, true)
+        val selectedFeedItems = AutoDownloadServiceImpl.selectFeedItems(feedPreferences.autoDownloadPreferences,
+            feedItems
+        )
+        assertEquals(4, selectedFeedItems.size)
     }
 
     @Test
-    fun `should download oldest N episodes`() {
+    fun `should sort items from new to old`() {
+        feedPreferences.autoDownloadPreferences = AutoDownload(feedItems.size, true, true)
+        val selectedFeedItems = AutoDownloadServiceImpl.selectFeedItems(feedPreferences.autoDownloadPreferences,
+            feedItems
+        )
+        assertEquals(feedItems.reversed(), selectedFeedItems)
+    }
 
+    @Test
+    fun `should sort items from old to new`() {
+        feedPreferences.autoDownloadPreferences = AutoDownload(feedItems.size, false, true)
+        val selectedFeedItems = AutoDownloadServiceImpl.selectFeedItems(feedPreferences.autoDownloadPreferences,
+            feedItems
+        )
+        assertEquals(feedItems, selectedFeedItems)
     }
 }
