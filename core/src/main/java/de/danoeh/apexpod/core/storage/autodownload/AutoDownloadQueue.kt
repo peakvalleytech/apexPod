f89d6b5e8f138de116bdceee93e3293054762c37
peakvalleytech <peakvalleytech@gmail.com>
2021-12-02 21:15:31 -0800

    Add AutoDownloadQueue

---
+++ core/src/main/java/de/danoeh/apexpod/core/storage/autodownload/AutoDownloadQueue.kt
@@ -0,0 +1,64 @@
+package de.danoeh.apexpod.core.storage.autodownload
+
+import de.danoeh.apexpod.core.util.FeedItemUtil
+import de.danoeh.apexpod.model.feed.FeedItem
+
+class AutoDownloadQueue(val items : MutableList<FeedItem>) {
+
+    fun getUnplayedItems(includeAll: Boolean): AutoDownloadQueue {
+        val unplayedItems = mutableListOf<FeedItem>()
+        if (!includeAll) {
+            items.forEach {
+                if (it.playState == FeedItem.NEW) {
+                    unplayedItems.add(it)
+                }
+            }
+        } else {
+            items.forEach {
+                if (it.playState == FeedItem.NEW
+                    || it.playState == FeedItem.UNPLAYED
+                ) {
+                    unplayedItems.add(it)
+                }
+            }
+        }
+        return AutoDownloadQueue(unplayedItems)
+    }
+
+    fun getDownloadable(
+        limit: Int
+    ): AutoDownloadQueue {
+        var i = 0
+        val downloadAbleSelectedItems: MutableList<FeedItem> = mutableListOf()
+
+        while (downloadAbleSelectedItems.size < limit && i < items.size) {
+            val item = items.get(i)
+            if (item.isAutoDownloadable() && !FeedItemUtil.isPlaying(item.getMedia())
+                && !item.getFeed().isLocalFeed()
+            ) {
+                downloadAbleSelectedItems.add(item)
+            }
+            ++i
+        }
+        return AutoDownloadQueue(downloadAbleSelectedItems)
+    }
+
+    fun sortByNewest(reverse: Boolean) {
+        items.sortBy { feedItem -> feedItem.pubDate }
+        if (!reverse) {
+            items.reverse()
+        }
+    }
+
+    fun size(): Int {
+        return items.size
+    }
+
+    fun subList(start: Int, size: Int): AutoDownloadQueue {
+        return AutoDownloadQueue(items.subList(start, size))
+    }
+
+    fun toList() : List<FeedItem> {
+        return items
+    }
+}
