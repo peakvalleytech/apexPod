f391c19f4a036dc4437405e0ec7d292f429c4c8c
peakvalleytech <peakvalleytech@gmail.com>
2022-01-20 17:34:57 -0800

    WIP

--- core/src/main/java/de/danoeh/apexpod/core/storage/database/PlayStatDao.kt
+++ core/src/main/java/de/danoeh/apexpod/core/storage/database/PlayStatDao.kt
@@ -3,11 +3,21 @@ package de.danoeh.apexpod.core.storage.database
 import de.danoeh.apexpod.model.stats.PlayStat
 import de.danoeh.apexpod.model.stats.PlayStatRange
 
-interface PlayStatDao {
+class PlayStatDao {
     fun createPlayStat(playStat : PlayStat) : Long
-    fun getAllPlayStats() : PlayStatRange
-    fun getAllByFeedItemId(feedItemId : Long) : PlayStatRange
-    fun getPlayStatsByRange(fromDateMillis : Long, toDateMillis : Long) : PlayStatRange
-    fun updatePlayStat(playStat : PlayStat)
-    fun deletePlayStat(playStat: PlayStat)
+    fun getAllPlayStats() : PlayStatRange {
+
+    }
+    fun getAllByFeedItemId(feedItemId : Long) : PlayStatRange {
+
+    }
+    fun getPlayStatsByRange(fromDateMillis : Long, toDateMillis : Long) : PlayStatRange {
+
+    }
+    fun updatePlayStat(playStat : PlayStat) {
+
+    }
+    fun deletePlayStat(playStat: PlayStat) {
+
+    }
 }
