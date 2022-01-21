92719bdb27e911a734497b584d9d69f327e2b502
peakvalleytech <peakvalleytech@gmail.com>
2022-01-20 22:26:56 -0800

    Update unit tests

--- core/src/test/java/de/danoeh/apexpod/core/storage/database/PlayStatDaoTest.java
+++ core/src/test/java/de/danoeh/apexpod/core/storage/database/PlayStatDaoTest.java
@@ -2,6 +2,7 @@ package de.danoeh.apexpod.core.storage.database;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static de.danoeh.apexpod.core.storage.DbTestUtils.saveFeedlist;
 
@@ -38,6 +39,11 @@ public class PlayStatDaoTest {
         PlayStatRange singleFeedList = new PlayStatRange(0, 100);
         PlayStatRange multiFeedList = new PlayStatRange(0, 100);
         public TestData() {
+            initSingleFeedList();
+            initMultiFeedList();
+        }
+
+        private void initSingleFeedList() {
             long feedItemId = 1;
             long feedId = 1;
             singleFeedList.add(new PlayStat(0, feedItemId, feedId,0, 1, 0, 0));
@@ -48,6 +54,14 @@ public class PlayStatDaoTest {
 
         }
 
+        private void initMultiFeedList() {
+            long feedItemId = 1;
+            multiFeedList.add(new PlayStat(0, feedItemId, 1,0, 1, 0, 0));
+            multiFeedList.add(new PlayStat(0, feedItemId, 2,2, 4, 0, 0));
+            multiFeedList.add(new PlayStat(0, feedItemId, 3,5, 8, 0, 0));
+            multiFeedList.add(new PlayStat(0, feedItemId, 4,9, 13, 0, 0));
+            multiFeedList.add(new PlayStat(0, feedItemId, 5,14, 19, 0, 0));
+        }
     }
     private TestData data;
     @Before
@@ -75,23 +89,31 @@ public class PlayStatDaoTest {
             assertEquals(  i + 1, expectedId);
         }
     }
-
     @Test
-    public void givenTableNotEmpty_whenGettingAllItems_shouldReturnAll() {
+    public void givenTableEmpty_whenGettingAllItems_shouldReturnNull() {
+        PlayStatRange playStatRange = playStatsDao.getAllPlayStats();
+        assertNull(playStatRange);
+    }
+    @Test
+    public void givenTableNotEmpty_whenGettingAllItems_shouldReturnPlayStatRange() {
         List<PlayStat> playStats = new ArrayList<>();
-        int numOfStats = data.singleFeedList.size();
+        int numOfStats = data.multiFeedList.size();
         for (int i = 0; i < numOfStats; ++i) {
-            playStats.add(data.singleFeedList.get(i));
+            playStats.add(data.multiFeedList.get(i));
         }
         for (int i = 0; i < numOfStats; ++i) {
             playStatsDao.createPlayStat(playStats.get(i));
         }
-        PlayStatRange createdPlayStats = playStatsDao.getAllPlayStats();
-        assertEquals(createdPlayStats.size(), playStats.size());
+        PlayStatRange playStatRange = playStatsDao.getAllPlayStats();
+        assertEquals(playStatRange.size(), playStats.size());
+        assertEquals(playStats.get(0).getStartTime(), playStatRange.getStart());
+        assertEquals(playStats.get(numOfStats - 1).getEndTime(), playStatRange.getEnd());
     }
 
+
+
     @Test
-    public void givenFeedItemId_whenValid_shouldReturnMatchingPlayStats() {
+    public void givenFeed_whenValid_shouldReturnMatchingPlayStats() {
 
     }
 
