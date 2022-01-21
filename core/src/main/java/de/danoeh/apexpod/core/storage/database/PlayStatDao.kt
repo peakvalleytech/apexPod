8f9fbcacc6ce475c1b8daf03e6c49cbab1eca756
peakvalleytech <peakvalleytech@gmail.com>
2022-01-20 18:23:31 -0800

    Update upgrade strcuture

--- core/src/main/java/de/danoeh/apexpod/core/storage/database/PlayStatDao.kt
+++ core/src/main/java/de/danoeh/apexpod/core/storage/database/PlayStatDao.kt
@@ -1,18 +1,28 @@
 package de.danoeh.apexpod.core.storage.database
 
+import android.database.sqlite.SQLiteDatabase
+import de.danoeh.apexpod.core.storage.ApexDBAdapter
 import de.danoeh.apexpod.model.stats.PlayStat
 import de.danoeh.apexpod.model.stats.PlayStatRange
 
 class PlayStatDao {
-    fun createPlayStat(playStat : PlayStat) : Long
-    fun getAllPlayStats() : PlayStatRange {
+    private var db: SQLiteDatabase
+    var dbAdapter: ApexDBAdapter
+    init {
+        dbAdapter = ApexDBAdapter.getInstance()
+        db = dbAdapter.db
+    }
+    fun createPlayStat(playStat : PlayStat) : Long {
 
+    }
+    fun getAllPlayStats() : PlayStatRange {
+        return PlayStatRange(0, 1)
     }
     fun getAllByFeedItemId(feedItemId : Long) : PlayStatRange {
-
+        return PlayStatRange(0, 1)
     }
     fun getPlayStatsByRange(fromDateMillis : Long, toDateMillis : Long) : PlayStatRange {
-
+        return PlayStatRange(0, 1)
     }
     fun updatePlayStat(playStat : PlayStat) {
 
--- core/src/main/java/de/danoeh/apexpod/core/storage/database/upgrades/Version2.kt
+++ core/src/main/java/de/danoeh/apexpod/core/storage/database/upgrades/Version2.kt
@@ -3,11 +3,8 @@ package de.danoeh.apexpod.core.storage.database.upgrades
 import android.database.sqlite.SQLiteDatabase
 import de.danoeh.apexpod.core.storage.database.DBUpgrade
 
-class Version1 : DBUpgrade {
-    override fun version() : Int {
-        return 1
-    }
+class Version2(val oldVersion : Int, val newVersion : Int) : DBUpgrade(oldVersion, newVersion) {
+    override fun upgrade(db: SQLiteDatabase) {
 
-    override fun upgrade(db: SQLiteDatabase, from: Int) {
     }
 }
