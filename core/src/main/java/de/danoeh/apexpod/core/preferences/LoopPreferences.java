f52cd8a3a1ed64b0f113da1d9df29ed04ccfb82e
peakvalleytech <peakvalleytech@gmail.com>
2022-03-29 11:53:08 -0700

    Resolve NPE issue in LoopPreferences

--- core/src/main/java/de/danoeh/apexpod/core/preferences/LoopPreferences.java
+++ core/src/main/java/de/danoeh/apexpod/core/preferences/LoopPreferences.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import androidx.preference.PreferenceManager;
 
 public class LoopPreferences {
+    private static Context context;
     private static SharedPreferences sharedPreferences;
     public static final String PREF_KEY_LOOP_ENABLED = "loop_enabled";
     public static final String PREF_KEY_LOOP_FEED_ID = "loop_feed_id";
@@ -13,38 +14,47 @@ public class LoopPreferences {
     public static final String PREF_KEY_LOOP_END = "loop_end";
 
     public static void init(Context context) {
+        LoopPreferences.context = context;
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
     }
 
     public static boolean isEnabled() {
-        return sharedPreferences.getBoolean(PREF_KEY_LOOP_ENABLED, false);
+        return getSharedPreferences().getBoolean(PREF_KEY_LOOP_ENABLED, false);
     }
 
     public static void setEnabled(boolean enabled) {
-        sharedPreferences.edit().putBoolean(PREF_KEY_LOOP_ENABLED, enabled).apply();
+        getSharedPreferences().edit().putBoolean(PREF_KEY_LOOP_ENABLED, enabled).apply();
     }
 
     public static long getFeedItemId() {
-        return sharedPreferences.getLong(PREF_KEY_LOOP_FEED_ID, 0);
+        return getSharedPreferences().getLong(PREF_KEY_LOOP_FEED_ID, 0);
     }
 
     public static void setFeedItemId(long feedId) {
-        sharedPreferences.edit().putLong(PREF_KEY_LOOP_FEED_ID, feedId).apply();
+        getSharedPreferences().edit().putLong(PREF_KEY_LOOP_FEED_ID, feedId).apply();
     }
 
     public static int getStart() {
-        return sharedPreferences.getInt(PREF_KEY_LOOP_START, 0);
+        return getSharedPreferences().getInt(PREF_KEY_LOOP_START, 0);
     }
 
     public static void setStart(int start) {
-        sharedPreferences.edit().putInt(PREF_KEY_LOOP_START, start).apply();
+        getSharedPreferences().edit().putInt(PREF_KEY_LOOP_START, start).apply();
     }
 
     public static int getEnd() {
-        return sharedPreferences.getInt(PREF_KEY_LOOP_END, 0);
+        return getSharedPreferences().getInt(PREF_KEY_LOOP_END, 0);
     }
 
     public static void setEnd(int end) {
-        sharedPreferences.edit().putInt(PREF_KEY_LOOP_END, end).apply();
+        getSharedPreferences().edit().putInt(PREF_KEY_LOOP_END, end).apply();
+    }
+
+    public static SharedPreferences getSharedPreferences() {
+        if (sharedPreferences == null) {
+            sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        }
+
+        return sharedPreferences;
     }
 }
